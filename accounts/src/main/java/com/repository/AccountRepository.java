53005634b73161e816ef73371a90a1170a019f80
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-15 17:03:32 +0300

    feat: add AccountRepository

---
+++ accounts/src/main/java/com/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.repository;
+
+import com.model.dto.CurrencyEnum;
+import com.model.entities.Account;
+import jdk.jfr.Registered;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    Optional<Account> findByUserIdAndCurrency(Long userId, CurrencyEnum currency);
+}
