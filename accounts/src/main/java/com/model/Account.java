ffe7f823ef9e062f07229e2a255b84fbc394d801
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-15 13:55:36 +0300

    feat: add User, Account

---
+++ accounts/src/main/java/com/model/Account.java
@@ -0,0 +1,28 @@
+package com.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "account", schema = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private CurrencyEnum currency;
+
+    @Column(name = "amount")
+    private Double value;
+}
--- accounts/src/main/java/com/model/User.java
+++ accounts/src/main/java/com/model/User.java
@@ -1,5 +1,32 @@
 package com.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "accounts")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String personName;
+    private LocalDate dateOfBirth;
+
+    public User(Long id) {
+        this.id = id;
+    }
+
 }
