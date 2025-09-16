edddf372513edb5d84f32931d7d2684870fd172d
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-16 15:17:32 +0300

    feat: add BlockerController

---
+++ blocker/src/main/java/com/controller/BlockerController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/validate")
+public class BlockerController {
+
+    private final BlockerService blockerService;
+
+    @GetMapping("")
+    @PreAuthorize("hasRole('ROLE_BLOCKER')")
+    public Boolean validate() throws AccountNotFoundException {
+        return blockerService.validate();
+    }
+}
