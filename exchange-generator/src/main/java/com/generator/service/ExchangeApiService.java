cba8c84e2e191a4301e6b3921e4cfb00ce2c2338
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-16 23:15:28 +0300

    feat: add ExchangeApiService

---
+++ exchange-generator/src/main/java/com/generator/service/ExchangeApiService.java
@@ -0,0 +1,25 @@
+package com.generator.service;
+
+import com.generator.dto.ExchangeDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class ExchangeApiService {
+
+    private final RestClient exchangeServiceClient;
+
+    public ExchangeApiService(RestClient.Builder builder) {
+        this.exchangeServiceClient = builder.baseUrl("http://exchange-api/api").build();
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public void setExchange(ExchangeDto exchangeDto) {
+        exchangeServiceClient.post()
+                .uri("/exchange")
+                .body(exchangeDto)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
