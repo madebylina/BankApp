435c334269b8274ec55e6b9763adc8f0af3cccef
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-16 19:37:13 +0300

    feat: add NotificationsApiService

---
+++ cash/src/main/java/com/cash/service/NotificationsApiService.java
@@ -0,0 +1,25 @@
+package com.cash.service;
+
+import com.cash.dto.NotificationDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class NotificationsApiService {
+
+    private final RestClient notificationsServiceClient;
+
+    public NotificationsApiService(RestClient.Builder builder) {
+        this.notificationsServiceClient = builder.baseUrl("http://notifications-api/api").build();
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public void notificate(NotificationDto notificationDto) {
+        notificationsServiceClient.post()
+                .uri("/notifications")
+                .body(notificationDto)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
