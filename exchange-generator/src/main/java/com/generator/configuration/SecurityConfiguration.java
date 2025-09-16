7f178a37658c6e6d24be4154e0fe2428cc6a4438
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-16 22:58:13 +0300

    feat: add SecurityConfiguration

---
+++ exchange-generator/src/main/java/com/generator/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.generator.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientService authorizedClientService
+    ) {
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
+
+        manager.setAuthorizedClientProvider(OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .refreshToken()
+                .build());
+        return manager;
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestClient.Builder restClientBuilder(OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        return RestClient.builder()
+                .requestInterceptor((request, body, execution) -> {
+                    OAuth2AuthorizeRequest authRequest = OAuth2AuthorizeRequest
+                            .withClientRegistrationId("keycloak")
+                            .principal("system")
+                            .build();
+
+                    OAuth2AuthorizedClient client = authorizedClientManager.authorize(authRequest);
+                    if (client == null) {
+                        throw new IllegalStateException("Не удалось получить OAuth2AuthorizedClient");
+                    }
+
+                    request.getHeaders().setBearerAuth(client.getAccessToken().getTokenValue());
+                    return execution.execute(request, body);
+                });
+    }
+}
