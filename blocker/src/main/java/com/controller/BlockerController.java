512a908703ba46f419e787b3e0c217ab4b7b1e9c
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-16 15:20:17 +0300

    feat: add BlockerService

--- blocker/src/main/java/com/controller/BlockerController.java
+++ blocker/src/main/java/com/controller/BlockerController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import com.service.BlockerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
