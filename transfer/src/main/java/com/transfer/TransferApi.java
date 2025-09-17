40ecad17cbd92e11945414762b0169871ddf6097
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 23:18:41 +0300

    feat: add Service

---
+++ transfer/src/main/java/com/transfer/TransferApi.java
@@ -0,0 +1,12 @@
+package com.transfer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferApi.class, args);
+    }
+
+}
