20f0cabe0836b5eb75ffae4fe20816bbc071c774
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 22:42:38 +0300

    feat: add dto

---
+++ transfer/src/main/java/com/transfer/dto/ExchangeDto.java
@@ -0,0 +1,14 @@
+package com.transfer.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeDto {
+
+    private CurrencyEnum currency;
+    private Double value;
+
+}
