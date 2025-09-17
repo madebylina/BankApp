46c45a1da654d9fff57aa2171def39252cee171d
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 13:18:22 +0300

    feat: add ExchangeController

---
+++ exchange/src/main/java/com/exchange/controller/ExchangeController.java
@@ -0,0 +1,27 @@
+package com.exchange.controller;
+
+import com.exchange.model.dto.CurrencyEnum;
+import com.exchange.model.dto.ExchangeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/exchange")
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('ROLE_EXCHANGE')")
+    public void setExchange(@RequestBody ExchangeDto exchangeDto) {
+        exchangeService.setExchange(exchangeDto);
+    }
+
+    @GetMapping("/{currency}")
+    @PreAuthorize("hasRole('ROLE_EXCHANGE')")
+    public Double getExchange(@PathVariable CurrencyEnum currencyEnum) {
+        return exchangeService.getExchange(currency);
+    }
+}
