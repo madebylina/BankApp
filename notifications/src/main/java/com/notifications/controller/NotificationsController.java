9488f29b2c81b14680608639a8a120c4addade58
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 21:43:06 +0300

    feat: add NotificationDto

---
+++ notifications/src/main/java/com/notifications/controller/NotificationsController.java
@@ -0,0 +1,23 @@
+package com.notifications.controller;
+
+import com.notifications.dto.NotificationDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notifications")
+public class NotificationsController {
+
+    private final NotificationsService notificationsService;
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('ROLE_NOTIFICATIONS')")
+    public void notificate(@RequestBody NotificationDto notificationDto) {
+        notificationsService.sendNotification(notificationDto);
+    }
+}
