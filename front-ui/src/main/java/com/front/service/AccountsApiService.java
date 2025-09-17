31017abe47a5418804ac1258b3fd9e38ec5c8987
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 20:00:00 +0300

    feat: add AccountsApiService

---
+++ front-ui/src/main/java/com/front/service/AccountsApiService.java
@@ -0,0 +1,73 @@
+package com.front.service;
+
+import com.front.dto.AccountDto;
+import com.front.dto.CurrencyEnum;
+import com.front.dto.UserDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class AccountsApiService {
+    private final RestClient accountsServiceClient;
+
+    public AccountsApiService(RestClient.Builder builder) {
+        this.accountsServiceClient = builder.baseUrl("http://accounts-api/api").build();
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public List<UserDto> getUsers() {
+        return accountsServiceClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/account/users")
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<UserDto>>() {
+                });
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public UserDto getUserByName(String username) {
+        return accountsServiceClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/account/user")
+                        .queryParam("username", username)
+                        .build())
+                .retrieve()
+                .body(UserDto.class);
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public AccountDto getAccountByUserAndCurrency(Long userId, CurrencyEnum currency) {
+        return accountsServiceClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/account/value/{userId}/{currency}")
+                        .build(userId, currency))
+                .retrieve()
+                .body(AccountDto.class);
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public AccountDto saveAccount(AccountDto accountDto) {
+        return accountsServiceClient.post()
+                .uri("/account")
+                .body(accountDto)
+                .retrieve()
+                .body(AccountDto.class);
+    }
+
+    @CircuitBreaker(name = "cbservice")
+    public void deleteAccount(AccountDto accountDto) {
+        accountsServiceClient.delete()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/account/{id}")
+                        .build(accountDto.getId()))
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+}
+
