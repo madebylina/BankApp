7f283d7242912407e5069be550d177b1502ea8a1
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-09-17 12:18:42 +0300

    feat: add Model

---
+++ exchange/src/main/java/com/exchange/model/dto/CurrencyEnum.java
@@ -0,0 +1,17 @@
+package com.exchange.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum CurrencyEnum {
+    RUB("Рубли"),
+    USD("Доллары"),
+    CNY("Юани");
+
+    private final String title;
+
+    CurrencyEnum(String title) {
+        this.title = title;
+    }
+
+}
---
+++ exchange/src/main/java/com/exchange/model/entities/Exchange.java
@@ -0,0 +1,23 @@
+package com.exchange.model.entities;
+
+import com.exchange.model.dto.CurrencyEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "exchange", schema = "exchange")
+public class Exchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private CurrencyEnum currency;
+
+    @Column(name = "amount")
+    private Double value;
+}
