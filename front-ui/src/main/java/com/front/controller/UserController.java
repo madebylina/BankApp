d9b161d1fea7ac958b103c8cc061c34e31939cb6
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-01 23:38:31 +0300

    feat: add MainController

--- front-ui/src/main/java/com/front/controller/UserController.java
+++ front-ui/src/main/java/com/front/controller/UserController.java
@@ -8,6 +8,7 @@ import com.front.service.CashApiService;
 import com.front.service.TransferApiService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,8 +22,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -112,6 +117,48 @@ public class UserController {
         }
         redirectAttributes.addFlashAttribute("transferErrors", transferErrors);
         redirectAttributes.addFlashAttribute("transferOtherErrors", transferOtherErrors);
+        return "redirect:/main";
+    }
+
+    @PostMapping("/{login}/editUserAccounts")
+    public String editUserAccounts(@PathVariable(name = "login") String login,
+                                   @RequestParam(name = "name") String name,
+                                   @RequestParam(name = "birthdate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthdate,
+                                   @RequestParam(name = "account", required = false) List<String> selectedCurrencies,
+                                   RedirectAttributes redirectAttributes) {
+
+        List<String> userAccountsErrors = new ArrayList<>();
+
+        UserDto userDto = accountsApiService.getUserByName(login);
+
+        if(Period.between(birthdate, LocalDate.now()).getYears() < 18){
+            userAccountsErrors.add("Вам должно быть больше 18 лет");
+        }else {
+            userDto.setPersonName(name);
+            userDto.setDateOfBirth(birthdate);
+            accountsApiService.saveUser(userDto);
+        }
+
+        Arrays.stream(CurrencyEnum.values())
+                .forEach(currency -> {
+                    AccountDto accountDto = accountsApiService.getAccountByUserAndCurrency(userDto.getId(), currency);
+                    if (accountDto.getExists()) {
+                        if (Objects.isNull(selectedCurrencies) || !selectedCurrencies.contains(accountDto.getCurrency().name())) {
+                            if(accountDto.getValue()==0) {
+                                accountsApiService.deleteAccount(accountDto);
+                            }else {
+                                userAccountsErrors.add("Баланс на счету %s не равен 0".formatted(currency.getTitle()));
+                            }
+                        }
+                    } else {
+                        if (!Objects.isNull(selectedCurrencies) && selectedCurrencies.contains(accountDto.getCurrency().name())) {
+                            accountDto.setExists(true);
+                            accountsApiService.saveAccount(accountDto);
+                        }
+                    }
+                });
+
+        redirectAttributes.addFlashAttribute("passwordErrors", userAccountsErrors);
         return "redirect:/main";
     }
 
