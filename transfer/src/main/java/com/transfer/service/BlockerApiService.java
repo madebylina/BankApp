d5d7d0703658928d2a197bc596327b9eaec0003e
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-16 12:31:28 +0300

    feat: add Kafka message

--- transfer/src/main/java/com/transfer/service/BlockerApiService.java
+++ transfer/src/main/java/com/transfer/service/BlockerApiService.java
@@ -10,13 +10,14 @@ public class BlockerApiService {
 
     private final RestClient blockerServiceClient;
 
+    // public BlockerApiService(RestClient.Builder builder, @Value("${appservices.blocker-api:http://blocker-api/api}") String baseUrl) {
     public BlockerApiService(RestClient.Builder builder,
-                             @Value("${appservices.blocker-api:http://blocker-api/api}") String baseUrl) {
+                             @Value("${appservices.blocker-api:http://localhost:8087/api}") String baseUrl) {
         this.blockerServiceClient = builder.baseUrl(baseUrl).build();
     }
 
     @CircuitBreaker(name = "cbservice")
-    public Boolean validate(){
+    public Boolean validate() {
         return blockerServiceClient.get()
                 .uri("/validate")
                 .retrieve()
