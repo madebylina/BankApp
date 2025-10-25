91acc3bc4dd9f03a233ba0bd7ecec2cabac35327
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-25 18:50:12 +0300

    feat: add metrics AccountsApiService

--- cash/src/main/java/com/cash/service/AccountsApiService.java
+++ cash/src/main/java/com/cash/service/AccountsApiService.java
@@ -3,6 +3,7 @@ package com.cash.service;
 import com.cash.dto.CashDto;
 import com.cash.dto.UserDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -15,9 +16,8 @@ public class AccountsApiService {
 
     private final RestClient accountServiceClient;
 
-    public AccountsApiService(RestClient.Builder builder,
-                              @Value("${appservices.accounts-api:http://accounts-api/api}") String baseUrl) {
-        this.accountServiceClient = builder.baseUrl(baseUrl).build();
+    public AccountsApiService(@Qualifier("accountsApiClient") RestClient accountsServiceClient) {
+        this.accountServiceClient = accountsServiceClient;
     }
 
     @CircuitBreaker(name = "cbservice")
