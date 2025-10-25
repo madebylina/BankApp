7328f1c1d0fed5f14be016cea5e5a7a8db688d45
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-25 19:22:30 +0300

    feat: add metrics ExchangeProducer

--- exchange-generator/src/main/java/com/generator/service/ExchangeProducer.java
+++ exchange-generator/src/main/java/com/generator/service/ExchangeProducer.java
@@ -2,7 +2,11 @@ package com.generator.service;
 
 import com.generator.configuration.OAuth2TokenProvider;
 import com.generator.dto.ExchangeDto;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,16 +15,28 @@ import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ExchangeProducer {
 
     private final OAuth2TokenProvider tokenProvider;
     private final KafkaTemplate<String, ExchangeDto> kafkaTemplate;
+    private final Tracer tracer;
+    private final Propagator propagator;
 
     public void setExchange(ExchangeDto exchangeDto) {
+        log.info("отправка курса валюты {}: {}", exchangeDto.getCurrency().name(), exchangeDto.getValue());
         String token = tokenProvider.getAccessToken();
         ProducerRecord<String, ExchangeDto> record =
                 new ProducerRecord<>("exchange", exchangeDto.getCurrency().name(), exchangeDto);
         record.headers().add("Authorization", ("Bearer " + token).getBytes(StandardCharsets.UTF_8));
+
+        Span currentSpan = tracer.currentSpan();
+        if (currentSpan != null) {
+            propagator.inject(currentSpan.context(), record.headers(), (headers, key, value) -> {
+                headers.add(key, value.getBytes(StandardCharsets.UTF_8));
+            });
+        }
+
         kafkaTemplate.send(record);
     }
 }
