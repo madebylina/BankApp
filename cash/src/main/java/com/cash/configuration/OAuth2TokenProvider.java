5668235e90cb7c5271995e63753bfca42207536e
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-10-25 18:42:12 +0300

    feat: add metrics OAuth2TokenProvider

---
+++ cash/src/main/java/com/cash/configuration/OAuth2TokenProvider.java
@@ -0,0 +1,27 @@
+package com.cash.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2TokenProvider {
+
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
+
+    public String getAccessToken() {
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("keycloak")
+                .principal("system")
+                .build();
+
+        OAuth2AuthorizedClient client = authorizedClientManager.authorize(authorizeRequest);
+        if (client == null) {
+            throw new IllegalStateException("Не удалось получить OAuth2 токен");
+        }
+        return client.getAccessToken().getTokenValue();
+    }
+}
